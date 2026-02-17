431767ec500750de5905800a6d9e0cacb43d9fd9
Obafemi Kazeem <obafemi.kazeem@cyberspace.net.ng>
2026-02-17 08:28:36 +0100

    Started work on department upload

---
+++ Models/Dto/DepartmentCoursesErrorModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EduReg.Models.Dto
+{
+    public class DepartmentCoursesErrorModel
+    {
+        public string CourseCode { get; set; }
+        public string DepartmentCode { get; set; }
+        public string Title { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
